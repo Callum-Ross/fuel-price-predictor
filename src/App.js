4d442837185c20f45f208eba96d08fe6ee91eb28
cross000 <39590588+cross000@users.noreply.github.com>
2019-10-11 14:54:47 +1000

    fuel price predictor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Select from "react-select";
+class Dropdown extends React.Component {
+  state = {
+    selectedOption: null
+  };
 
+  handleChange = selectedOption => {
+    this.setState({ selectedOption });
+    this.props.selected(selectedOption);
+    console.log(`Option selected:`, selectedOption);
+  };
+
+  render() {
+    console.log("dfsdfsd");
+    const options = [];
+    // for (var i in this.props.tag) {
+    //   let myObj = {};
+    //   myObj.value = this.props.tag[i].value;
+    //   myObj.label = this.props.tag[i].label;
+    //   options.push(myObj);
+    // }
+    // console.log(options);
+
+    const { selectedOption } = this.state;
+
+    return (
+      <div
+        style={{
+          width: "30%",
+          paddingLeft: "2.5%",
+          zIndex: "auto"
+          // position: "absolute"
+        }}
+      >
+        <Select
+          style={{ zIndex: "auto" }}
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={options}
+          placeholder="Please select a searchable tag or leave empty to search for all..."
+          isMulti
+        />
+      </div>
+    );
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex" }}>
+      <Dropdown />
+      <Dropdown />
+      <Dropdown />
     </div>
   );
 }
