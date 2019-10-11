d08cd4108587a204de31a92839bd2aeda18bc53c
cross000 <39590588+cross000@users.noreply.github.com>
2019-10-11 21:41:55 +1000

    all inputs in place

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Select from "react-select";
-import siteDetails from "./api";
+import makeAnimated from "react-select/animated";
+import { siteDetails, fuelTypes } from "./api";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+class TimePeriod extends React.Component {
+  state = {
+    startDate: new Date()
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+  };
+
+  render() {
+    return (
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+      />
+    );
+  }
+}
 class Dropdown extends React.Component {
   state = {
     selectedOption: null
@@ -9,13 +32,42 @@ class Dropdown extends React.Component {
 
   handleChange = selectedOption => {
     this.setState({ selectedOption });
-    this.props.selected(selectedOption);
+
     console.log(`Option selected:`, selectedOption);
   };
 
   render() {
-    console.log("dfsdfsd");
+    const animatedComponents = makeAnimated();
+    let displayed = this.props.displayed;
     const options = [];
+    let type = "Please select a time to predict, leave empty to select all...";
+
+    if (typeof displayed.S !== "undefined") {
+      if (this.props.props === 1) {
+        type =
+          "Please select a site location to predict, leave empty to select all..";
+        for (var site in displayed.S) {
+          let label = displayed.S[site].N;
+          let value = displayed.S[site].A;
+          let option = { value: value, label: label };
+          options.push(option);
+        }
+      }
+    }
+    if (typeof displayed.Fuels !== "undefined") {
+      if (this.props.props === 2) {
+        console.log("dfsdf");
+        type =
+          "Please select a Fuel Type to predict, leave empty to select all..";
+        for (var fuel in displayed.Fuels) {
+          let label = displayed.Fuels[fuel].Name;
+          let value = displayed.Fuels[fuel].FuelId;
+          let option = { value: value, label: label };
+          options.push(option);
+        }
+      }
+    }
+
     // for (var i in this.props.tag) {
     //   let myObj = {};
     //   myObj.value = this.props.tag[i].value;
@@ -36,11 +88,13 @@ class Dropdown extends React.Component {
         }}
       >
         <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
           style={{ zIndex: "auto" }}
           value={selectedOption}
           onChange={this.handleChange}
           options={options}
-          placeholder="Please select a searchable tag or leave empty to search for all..."
+          placeholder={type}
           isMulti
         />
       </div>
@@ -48,15 +102,33 @@ class Dropdown extends React.Component {
   }
 }
 function App() {
-  const [sites, setSites] = useState([""]);
+  const [sites, setSites] = useState([]);
+  const [types, setTypes] = useState([]);
+
   useEffect(() => {
     siteDetails().then(res => setSites(res));
   }, []);
+  useEffect(() => {
+    fuelTypes().then(res => setTypes(res));
+  }, []);
+  console.log(types);
   return (
-    <div className="App" style={{ display: "flex" }}>
-      <Dropdown />
-      <Dropdown />
-      <Dropdown />
+    <div className="App">
+      <div style={{ display: "flex" }}>
+        <Dropdown props={1} displayed={sites} />
+        <Dropdown props={2} displayed={types} />
+        <Dropdown props={3} displayed={sites} />
+      </div>
+      <div
+        style={{
+          justifyContent: "center",
+          display: "flex"
+        }}
+      >
+        <TimePeriod />
+
+        <TimePeriod />
+      </div>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export default async function siteDetails() {
+export async function siteDetails() {
   const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
   const url = `https://fppdirectapi-prod.fuelpricesqld.com.au/Subscriber/GetFullSiteDetails?countryId=21&geoRegionLevel=3&geoRegionId=1`;
@@ -16,7 +16,29 @@ export default async function siteDetails() {
         "FPDAPI SubscriberToken=ea92a388-5358-44e1-a765-6aa9c4cb2cd0"
     }
   })
-    .then(response => response.text())
+    .then(response => response.json())
     .then(contents => (data = contents));
+  return data;
+}
+export async function fuelTypes() {
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+  const url = `https://fppdirectapi-prod.fuelpricesqld.com.au/Subscriber/GetCountryFuelTypes?countryId=21`;
+  let data = [];
+  await fetch(proxyurl + url, {
+    method: "GET", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+      // prettier-ignore
+      "Authorization":
+        "FPDAPI SubscriberToken=ea92a388-5358-44e1-a765-6aa9c4cb2cd0"
+    }
+  })
+    .then(response => response.json())
+    .then(contents => (data = contents));
+  console.log(data);
   return data;
 }
